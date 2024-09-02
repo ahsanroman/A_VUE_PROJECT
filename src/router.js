a882c2842655143bff1e0c82f862422b96e9d509
Ahsan Roman <41336480+ahsanroman@users.noreply.github.com>
2024-09-03 02:52:05 +0500

    18_A_VUE_PROJECT_IS_COMPLETED

--- src/router.js
+++ src/router.js
@@ -1,13 +1,20 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistation from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = defineAsyncComponent(() => import('./pages/coaches/CoachDetail.vue'));
+const CoachRegistration = defineAsyncComponent(() => import('./pages/coaches/CoachRegistration.vue'));
+const ContactCoach = defineAsyncComponent(() => import('./pages/requests/ContactCoach.vue'));
+const RequestsReceived = defineAsyncComponent(() => import('./pages/requests/RequestsReceived.vue'));
+const UserAuth = defineAsyncComponent(() => import('./pages/auth/UserAuth.vue'));
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,7 +29,7 @@ const router = createRouter({
         { path: 'contact', component: ContactCoach } // /coaches/c1/contact
       ]
     },
-    { path: '/register', component: CoachRegistation, meta: { requiresAuth: true } },
+    { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } },
     { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
     { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
     { path: '/:notFound(.*)', component: NotFound }
